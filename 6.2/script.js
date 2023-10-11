876a81354792e470de64c033adb6c6a740d47e64
Reyha4n <143797857+Reyha4n@users.noreply.github.com>
2023-10-11 11:23:07 +0200

    opdracht calculator

---
+++ 6.2/script.js
@@ -0,0 +1,20 @@
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+calculator(15, 16);
+
+function multiplyByFive (num, num2) {
+    let answer = num * num2;
+    console.log(answer);
+}
+
+multiplyByFive(31, 5)
+
+function calculateMinutes (num, num2) {
+    let answer = num / num2;
+    console.log(answer);
+}
+
+calculateMinutes(540, 60)
