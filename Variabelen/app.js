475208e615b90f230754173103ec188638522397
Reyha4n <143797857+Reyha4n@users.noreply.github.com>
2023-11-07 14:07:01 +0100

    Update

--- Variabelen/app.js
+++ Variabelen/app.js
@@ -5,3 +5,12 @@ if(budget > 60){
 } else {
     console.log("Helaas, te weinig geldt")
 }
+
+function myFunction() {
+    let product = prompt("Hoeveel kost het product dat je wilt kopen?", "100", "60");
+    if (product > 60) {
+      console.log("U heeft genoeg geld")
+    } else {
+        console.log("U heeft niet genoeg geld")
+    }
+  }
