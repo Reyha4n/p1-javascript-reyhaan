64e187ec7f6df8de0119db0218a75f5477a05634
Reyha4n <143797857+Reyha4n@users.noreply.github.com>
2023-11-08 11:18:35 +0100

    Opdracht 7.2

--- Week 7/Opdracht 7-1 IF else statement/script.js
+++ Week 7/Opdracht 7-1 IF else statement/script.js
@@ -4,5 +4,15 @@ const product = 60
 if(budget > product){
     console.log("U heeft genoeg geld!")
 } else {
-    console.log("Helaas, te weinig geldt")
+    console.log("Helaas, te weinig geld")
 }
+
+function vraag(){
+    let Antwoord = prompt("Hoeveel kost het product dat je wilt kopen?");
+    if (Antwoord > product){
+        console.log("U heeft genoeg geld!")
+    } else {
+        console.log("Helaas, te weinig geld")
+    }
+}
+
